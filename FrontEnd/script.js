fcae5579ce331854cdbd4bfd21adc7e21f1ae305
Ndo18 <samuel.dmb@outlook.fr>
2023-10-28 18:50:37 +0200

    🐛 Filters fixed

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -34,7 +34,6 @@ async function genererProjets(){
         const response = await fetch("http://localhost:5678/api/works")
         const tabProjets = await response.json()
         const divFiltres = document.querySelector(".filtres")
-        const gallery = document.querySelector(".gallery")
         
         /* BOUTON TOUS */
         const btnTous = document.createElement("button")
@@ -68,13 +67,29 @@ async function genererProjets(){
         btnFiltres.forEach((btn) => {
             btn.addEventListener("click", (event) => {
                 document.querySelector(".gallery").innerHTML=""
-                const tabFilter = tabProjets.filter((tab) => tab.categoryId === event.target.getAttribute("data-id"));
-
-                tabFilter.forEach((tabProjets) => {
-                    console.log(tabProjets)
+                const tabFilter = tabProjets.filter((tab) => tab.categoryId == event.target.getAttribute("data-id"));
+                console.log(tabFilter)
+                
+                tabFilter.forEach((tabFilter) => {
+                    /* CRÉATION DE LA CLASSE GALLERY */
+                    const gallery = document.querySelector(".gallery")
+                    
+                    /* CRÉATION DE LA BALISE FIGURE */
+                    const figure = document.createElement("figure")
+                    
+                    /* CRÉATION DE L'ÉLÉMENT IMAGE */
+                    const imageElement = document.createElement("img")
+                    imageElement.src = tabFilter.imageUrl
+                    
+                    /* CRÉATION DE LA BALISE FIGCAPTION */
+                    const figcaption = document.createElement("figcaption")
+                    figcaption.innerText = tabFilter.title
+                    
+                    /* AJOUT DES ÉLÉMENTS À LA STRUCTURE HTML */
+                    figure.appendChild(imageElement)
+                    figure.appendChild(figcaption)
+                    gallery.appendChild(figure)
                 })
-
-                console.log(tabFilter);
                 
             })
             
