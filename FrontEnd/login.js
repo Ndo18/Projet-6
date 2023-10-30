5da0ca7270992af237996ab2ad70deac648a3468
Ndo18 <samuel.dmb@outlook.fr>
2023-10-30 12:47:51 +0100

    🚧 token localstorage

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -9,6 +9,11 @@ function connexion() {
         //     headers : {"Content-Type": "application/json"},
         //     body 
         // })
+
+        /*TOKEN LOCALSTORAGE*/
+        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
+        const valeurToken = JSON.stringify(token)
+        window.localStorage.setItem("token", valeurToken)
         
         const email = document.getElementById("email")
         const password = document.getElementById("pass")
