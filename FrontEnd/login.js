21fc70d8d7c03ee418c26cde5735ed7a892fa079
Ndo18 <samuel.dmb@outlook.fr>
2023-11-02 16:33:18 +0100

    ✅ 🚧 💄 token localstorage + LogIn page edited

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,28 +1,30 @@
-function connexion() {
-
+async function connexion() {
+    
     const form = document.querySelector("form")
     
-    form.addEventListener("submit", (event) =>{
+    form.addEventListener("submit", async (event) =>{
         event.preventDefault()
-        // fetch("http://localhost:5678/api/users/login", {
-        //     method : "POST",
-        //     headers : {"Content-Type": "application/json"},
-        //     body 
-        // })
-
-        /*TOKEN LOCALSTORAGE*/
-        const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTY1MTg3NDkzOSwiZXhwIjoxNjUxOTYxMzM5fQ.JGN1p8YIfR-M-5eQ-Ypy6Ima5cKA4VbfL2xMr2MgHm4"
-        const valeurToken = JSON.stringify(token)
-        window.localStorage.setItem("token", valeurToken)
         
-        const email = document.getElementById("email")
-        const password = document.getElementById("pass")
-    
+        const email = document.getElementById("email").value
+        const password = document.getElementById("pass").value
+        
         if(email === "" || password === ""){
             console.log("le champs est vide");
         } else {
-            console.log(email.value)
-            console.log(password.value);
+            console.log(email)
+            console.log(password);
+            const reponse = await fetch("http://localhost:5678/api/users/login", {
+                method : "POST",
+                body : JSON.stringify({
+                    "email" : email,
+                    "password" : password,
+                }),
+                headers : {"Content-Type": "application/json"},
+            })
+            const body = await reponse.json()
+            console.log(body);
+            window.localStorage.setItem("token", body.token)
+            window.location.href="./"
         }
     })
 }
---
+++ FrontEnd/settingsmode.js
@@ -0,0 +1,47 @@
+const stockToken = localStorage.getItem("token")
+
+if(stockToken == ""){
+    console.log("vous n'êtes pas connecté");
+} else {
+    //BARRE MODE EDITION
+    const bandEdition = document.getElementById("band")
+    bandEdition.classList.add("editionmode")
+    //ÎCONE
+    const iconeModif = document.createElement("i")
+    iconeModif.classList.add("fa-regular", "fa-pen-to-square")
+    //TEXT
+    const textEdition = document.createElement("p")
+    textEdition.innerText = " Mode édition"
+
+    textEdition.prepend(iconeModif)
+    bandEdition.appendChild(textEdition)
+    // console.log(iconeModif);
+
+    console.log("vous êtes connecté");
+
+    //MES PROJETS
+    const titreProjets = document.querySelector(".titreprojets")
+    const textEditionProjets = document.createElement("p")
+    textEditionProjets.innerText = " Modifier"
+    const iconeModif2 = document.createElement("i")
+    iconeModif2.classList.add("fa-regular", "fa-pen-to-square")
+
+    textEditionProjets.prepend(iconeModif2)
+    titreProjets.appendChild(textEditionProjets)
+    // console.log(titreProjets);
+
+    //RETRAIT FILTRES
+
+    const mesFiltres = document.querySelector(".filtres")
+    mesFiltres.remove()
+
+    // console.log(mesFiltres);
+
+    //MODIFIER BOUTON LOGIN LOGOUT
+
+    const logoutBtn = document.getElementById("textlogout")
+    logoutBtn.innerText = "logout"
+
+    // console.log(logoutBtn);
+
+}
